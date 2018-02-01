dd9dfc4351e2228d184de1a796706d1b388f167a
Michael Prather <mprather1@gmail.com>
2018-02-01 05:57:21 +0000

    new

---
+++ app/main.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { HashRouter as Router, Route } from 'react-router-dom'
+import Navigation from './components/Navigation.jsx'
+import Home from './components/Home.jsx'
+import Table from './components/Table.jsx'
+
+global.jQuery = require('jquery')
+require('bootstrap')
+require('../public/css/style.scss')
+
+ReactDOM.render((
+  <Router>
+    <div>
+      <Navigation />
+      <div className='container-fluid'>
+        <Route exact path='/home' component={Home} />
+        <Route exact path='/table' component={Table} />
+      </div>
+    </div>
+  </Router>
+), document.getElementById('container'))
